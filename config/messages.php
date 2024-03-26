e0866691987c1030bfb499d857fd043064fee7bc
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-03-26 18:14:59 +0100

    messages

---
+++ config/messages.php
@@ -0,0 +1,55 @@
+<?php
+
+return 
+[
+    [
+        'name' => 'Vincenzo',
+        'email' =>'vincenzo@gmail.com',
+        'message' =>'Ciao sono interessato '
+    ],
+    [
+        'name' => 'Diego',
+        'email' =>'diego@gmail.com',
+        'message' =>'Ciao è incluso il soggiorno '
+    ],
+    [
+        'name' => 'Riccardo',
+        'email' =>'riccardo@gmail.com',
+        'message' =>'Buonasera vorrei prenotare '
+    ],
+    [
+        'name' => 'Giovanni',
+        'email' =>'giovanni@gmail.com',
+        'message' =>'Per il posto macchina? '
+    ],
+    [
+        'name' => 'Matteo',
+        'email' =>'matteo@gmail.com',
+        'message' =>'Quanto è la caparra? '
+    ],
+    [
+        'name' => 'Salvatore',
+        'email' =>'salvatore@gmail.com',
+        'message' =>'quanto costa il servizio navettà? '
+    ],
+    [
+        'name' => 'Ludovica',
+        'email' =>'ludovica@gmail.com',
+        'message' =>'A che ora è il check-in? '
+    ],
+    [
+        'name' => 'Prisco',
+        'email' =>'prisco@gmail.com',
+        'message' =>'A che ora è il check-out? '
+    ],
+    [
+        'name' => 'Alice',
+        'email' =>'alice@gmail.com',
+        'message' =>'quanto costano le tassse di soggiorno? '
+    ],
+    [
+        'name' => 'Antonio',
+        'email' =>'antonio@gmail.com',
+        'message' =>' Si puo avere la cancellazione gratuita'
+    ],
+];
