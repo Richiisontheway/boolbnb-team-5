b8c5fc02521050e12dfcf55be61244f031328784
GiordanoDiego <diegomgiordano96@gmail.com>
2024-03-28 13:00:09 +0100

    fix show services

--- resources/views/admin/services/show.blade.php
+++ resources/views/admin/services/show.blade.php
@@ -4,13 +4,13 @@
 
 @section('main-content')
     <h1>{{ $service->title }}</h1>
-<p>{{ $service->description }}</p>
+    {{-- <p>{{ $service->description }}</p> --}}
 
-<h2>Appartamenti che offrono questo servizio:</h2>
-<ul>
-    @foreach($apartments as $apartment)
-        <li>{{ $apartment->name }}</li>
-    @endforeach
-</ul>
+    <h2>Appartamenti che offrono questo servizio:</h2>
+    <ul>
+        @foreach($apartments as $apartment)
+            <li>{{ $apartment->title }}</li>
+        @endforeach
+    </ul>
 
 @endsection
