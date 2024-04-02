d641c7893b115f4a5f0b8fc7dfa97e834e8eedbe
Richiisontheway <mineiriccardo@gmail.com>
2024-04-02 17:55:40 +0200

    add filter to contacts

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -9,18 +9,15 @@
 
     <div class="row g-0">
 
-        <form action="{{ route('admin.contacts.index') }}" method="GET">
-            <div class="input-group my-3">
-                <span class="input-group-text">Cerca per Nome</span>
-                <input type="text" aria-label="First name" class="form-control" id="name" name="name" value="{{ request()->input('name') }}">
-                <span class="input-group-text">Cerca per Mail</span>
-                <input type="text" aria-label="Last name" class="form-control" id="email" name="email" value="{{ request()->input('email') }}">
-                <button type="submit" class="btn btn-primary">
-                    Cerca
-                </button>    
-            </div>
-        </form>
-
+        <div class="input-group my-3">
+            <span class="input-group-text">Cerca per Nome</span>
+            <input type="text" aria-label="First name" class="form-control" id="name" name="name" value="{{ request()->input('name') }}" autocomplete="off">
+            
+            <span class="input-group-text">Cerca per Mail</span>
+            <input type="text" aria-label="Last name" class="form-control" id="email" name="email" value="{{ request()->input('email') }}" autocomplete="off">
+                
+        </div>
+       
         @foreach ($contacts as $contact)
             <div class="d-flex justify-content-center col-12 col-xs-6 col-sm-4 col-md-3 mb-3">
                 <div class="card m-1">
@@ -44,6 +41,42 @@
                 </div>
             </div>
         @endforeach
+       
     </div>
 
 @endsection
+<script>
+    //fa l evento sul caricamento del dom
+   document.addEventListener('DOMContentLoaded', function() {
+    let name_filter = document.getElementById('name');
+    let email_filter = document.getElementById('email');
+
+    name_filter.addEventListener('input', function() {
+        filterContacts(name_filter.value.toLowerCase(), email_filter.value.toLowerCase());
+    });
+
+    email_filter.addEventListener('input', function() {
+        filterContacts(name_filter.value.toLowerCase(), email_filter.value.toLowerCase());
+    });
+
+    function filterContacts(nameFilter, emailFilter) {
+        let cards = document.querySelectorAll('.card');
+
+        cards.forEach(function(card) {
+            let name = card.querySelector('h3').textContent.toLowerCase();
+            let email = card.querySelector('h5').textContent.toLowerCase();
+
+            let nameMatch = name.includes(nameFilter);
+            let emailMatch = email.includes(emailFilter);
+
+            if (nameMatch && emailMatch) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+});
+
+
+</script>
