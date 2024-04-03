ca9627b0d2ec1ce0290bc207576f80b0273dd7d8
GiordanoDiego <diegomgiordano96@gmail.com>
2024-04-03 10:08:39 +0200

    added delete button into show apartment

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -17,7 +17,7 @@
 
                         <div class="mt-3">
                             <h4>
-                                {{ $apartment->city }}, {{ $apartment->address }}. {{$apartment->title}}
+                                {{$apartment->title}}, {{ $apartment->address }}. 
                             </h4>
                             <p>
                                 {{ $apartment->n_rooms }} Stanze · {{ $apartment->n_beds }} Letti · {{ $apartment->n_baths }} Bagni · {{ $apartment->mq }} m2
@@ -51,6 +51,44 @@
                                     @else
                                         <a href="{{ route('admin.apartments.showSponsorizeForm', ['slug' => $apartment->slug]) }}" class="btn btn-primary">Sponsorizza</a>
                                     @endif
+                                </div>
+                                <div>
+                                    <button class="erase-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop-{{ $apartment->slug }}">
+                                        <i class="fa-solid fa-eraser"></i>
+                                    </button>
+        
+                                    <div class="modal fade" id="staticBackdrop-{{ $apartment->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                                                        Eliminazione Appartamento
+                                                    </h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Sei sicuto di voler eliminare: <b> {{ $apartment->title }} </b> ?
+                                                </div>
+                                                <div class="modal-footer">
+         
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+                                                    <form 
+                                                    action="{{ route('admin.apartments.destroy', ['apartment' => $apartment]) }}" 
+                                                    method="POST">
+                                                   
+                                                    @csrf
+                                                    @method('DELETE')
+                                                        <button 
+                                                        type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                            Elimina
+                                                        </button>
+                                                    </form>
+        
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>      
                                 </div>
                             </div>
                         </div>
