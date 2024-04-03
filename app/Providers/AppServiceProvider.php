efe5e1a5e3fad7775ff799d2a9dc810ab5881ff5
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-04-03 10:35:39 +0200

    edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
-        \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
-        \Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
-        \Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
-        \Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
+        // \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
+        // \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
+        // \Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
+        // \Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
+        // \Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
     }
 }
