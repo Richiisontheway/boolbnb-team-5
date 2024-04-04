eae6a7afc64b5a982b9d9a9d49eb9d6a69ab9724
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-04-04 11:54:31 +0200

    views e messages aggiornati

--- config/messages.php
+++ config/messages.php
@@ -70,4 +70,250 @@ return
         'message' =>' Si puo avere la cancellazione gratuita',
         'apartment_id' => '7'
     ],
+    [
+        'name' => 'John White',
+        'email' => 'john.white@example.com',
+        'message' => 'Buongiorno, vorrei avere maggiori informazioni sull\'appartamento.',
+        'apartment_id' => 12
+    ],
+    [
+        'name' => 'Anna Green',
+        'email' => 'anna.green@example.com',
+        'message' => 'Salve, sono interessata all\'appartamento. Potete darmi ulteriori dettagli?',
+        'apartment_id' => 35
+    ],
+    [
+        'name' => 'Luke Red',
+        'email' => 'luke.red@example.com',
+        'message' => 'Ciao, mi piacerebbe prenotare una visita per l\'appartamento.',
+        'apartment_id' => 42
+    ],
+    [
+        'name' => 'Mary Black',
+        'email' => 'mary.black@example.com',
+        'message' => 'Buonasera, vorrei sapere se l\'appartamento è disponibile per il mese prossimo.',
+        'apartment_id' => 17
+    ],
+    [
+        'name' => 'Simon Yellow',
+        'email' => 'simon.yellow@example.com',
+        'message' => 'Salve, sto cercando un appartamento con almeno due camere da letto. Questo appartamento soddisfa tale requisito?',
+        'apartment_id' => 25
+    ],
+    [
+        'name' => 'Julia Brown',
+        'email' => 'julia.brown@example.com',
+        'message' => 'Buongiorno, vorrei organizzare una visita per l\'appartamento.',
+        'apartment_id' => 10
+    ],
+    [
+        'name' => 'Mark Blue',
+        'email' => 'mark.blue@example.com',
+        'message' => 'Ciao, sto cercando un appartamento in zona centrale. Questo appartamento è in centro?',
+        'apartment_id' => 30
+    ],
+    [
+        'name' => 'Sarah Yellow',
+        'email' => 'sarah.yellow@example.com',
+        'message' => 'Salve, vorrei avere informazioni sui costi aggiuntivi associati all\'affitto dell\'appartamento.',
+        'apartment_id' => 18
+    ],
+    [
+        'name' => 'Matthew Pink',
+        'email' => 'matthew.pink@example.com',
+        'message' => 'Buonasera, sono interessato all\'appartamento. È possibile organizzare una visita?',
+        'apartment_id' => 38
+    ],
+    [
+        'name' => 'Eliza Violet',
+        'email' => 'eliza.violet@example.com',
+        'message' => 'Ciao, vorrei sapere se è possibile affittare l\'appartamento per un periodo di sei mesi.',
+        'apartment_id' => 22
+    ],
+    [
+        'name' => 'Alexander Celeste',
+        'email' => 'alexander.celeste@example.com',
+        'message' => 'Salve, vorrei ricevere informazioni sui servizi inclusi nell\'affitto dell\'appartamento.',
+        'apartment_id' => 14
+    ],
+    [
+        'name' => 'Valentina Orange',
+        'email' => 'valentina.orange@example.com',
+        'message' => 'Buongiorno, vorrei sapere se l\'appartamento è arredato.',
+        'apartment_id' => 28
+    ],
+    [
+        'name' => 'David White',
+        'email' => 'david.white@example.com',
+        'message' => 'Ciao, sono interessato all\'appartamento. Potete fornire informazioni sulla disponibilità?',
+        'apartment_id' => 11
+    ],
+    [
+        'name' => 'Clara Yellow',
+        'email' => 'clara.yellow@example.com',
+        'message' => 'Salve, vorrei prenotare una visita per l\'appartamento il prima possibile.',
+        'apartment_id' => 32
+    ],
+    [
+        'name' => 'Fabio Brown',
+        'email' => 'fabio.brown@example.com',
+        'message' => 'Buongiorno, vorrei sapere se è possibile concordare un periodo di affitto più lungo del solito.',
+        'apartment_id' => 19
+    ],
+    [
+        'name' => 'Eleanor Violet',
+        'email' => 'eleanor.violet@example.com',
+        'message' => 'Ciao, vorrei ricevere ulteriori dettagli sull\'appartamento.',
+        'apartment_id' => 15
+    ],
+    [
+        'name' => 'Francesco Orange',
+        'email' => 'francesco.orange@example.com',
+        'message' => 'Salve, vorrei sapere se sono inclusi nel prezzo anche i costi di gestione condominiale.',
+        'apartment_id' => 24
+    ],
+    [
+        'name' => 'Roberta Red',
+        'email' => 'roberta.red@example.com',
+        'message' => 'Buonasera, vorrei avere informazioni sulla durata minima dell\'affitto per questo appartamento.',
+        'apartment_id' => 26
+    ],
+    [
+        'name' => 'Daniel Gray',
+        'email' => 'daniel.gray@example.com',
+        'message' => 'Buongiorno, sono interessato all\'appartamento. Posso prenotare una visita?',
+        'apartment_id' => 27
+    ],
+    [
+        'name' => 'Sophia Black',
+        'email' => 'sophia.black@example.com',
+        'message' => 'Salve, vorrei maggiori dettagli sull\'appartamento in questione.',
+        'apartment_id' => 13
+    ],
+    [
+        'name' => 'Oliver Green',
+        'email' => 'oliver.green@example.com',
+        'message' => 'Ciao, potete fornire informazioni sui servizi inclusi nell\'affitto?',
+        'apartment_id' => 21
+    ],
+    [
+        'name' => 'Emily Brown',
+        'email' => 'emily.brown@example.com',
+        'message' => 'Buongiorno, vorrei sapere se l\'appartamento è disponibile per una visita oggi.',
+        'apartment_id' => 16
+    ],
+    [
+        'name' => 'William White',
+        'email' => 'william.white@example.com',
+        'message' => 'Salve, vorrei sapere se l\'appartamento è arredato.',
+        'apartment_id' => 23
+    ],
+    [
+        'name' => 'Amelia Blue',
+        'email' => 'amelia.blue@example.com',
+        'message' => 'Ciao, sono interessata all\'appartamento. È possibile avere una copia del contratto di locazione?',
+        'apartment_id' => 39
+    ],
+    [
+        'name' => 'James Red',
+        'email' => 'james.red@example.com',
+        'message' => 'Buongiorno, vorrei sapere se è inclusa una lavatrice nell\'appartamento.',
+        'apartment_id' => 37
+    ],
+    [
+        'name' => 'Charlotte Green',
+        'email' => 'charlotte.green@example.com',
+        'message' => 'Salve, vorrei avere informazioni sul trasporto pubblico nelle vicinanze dell\'appartamento.',
+        'apartment_id' => 20
+    ],
+    [
+        'name' => 'Alexander Gray',
+        'email' => 'alexander.gray@example.com',
+        'message' => 'Ciao, sono interessato all\'appartamento. Quali sono i costi di affitto mensili?',
+        'apartment_id' => 33
+    ],
+    [
+        'name' => 'Lily Brown',
+        'email' => 'lily.brown@example.com',
+        'message' => 'Buongiorno, vorrei sapere se è possibile affittare l\'appartamento a lungo termine.',
+        'apartment_id' => 36
+    ],
+    [
+        'name' => 'Michael White',
+        'email' => 'michael.white@example.com',
+        'message' => 'Salve, sono interessato all\'appartamento. È possibile organizzare una visita virtuale?',
+        'apartment_id' => 41
+    ],
+    [
+        'name' => 'Grace Black',
+        'email' => 'grace.black@example.com',
+        'message' => 'Ciao, vorrei sapere se sono ammessi animali domestici nell\'appartamento.',
+        'apartment_id' => 43
+    ],
+    [
+        'name' => 'Daniel Green',
+        'email' => 'daniel.green@example.com',
+        'message' => 'Buongiorno, vorrei sapere se l\'appartamento è dotato di aria condizionata.',
+        'apartment_id' => 44
+    ],
+    [
+        'name' => 'Emma Brown',
+        'email' => 'emma.brown@example.com',
+        'message' => 'Salve, vorrei sapere se è possibile affittare l\'appartamento anche per brevi periodi.',
+        'apartment_id' => 45
+    ],
+    [
+        'name' => 'Joseph Gray',
+        'email' => 'joseph.gray@example.com',
+        'message' => 'Ciao, sono interessato all\'appartamento. Qual è la durata minima dell\'affitto?',
+        'apartment_id' => 46
+    ],
+    [
+        'name' => 'Mia White',
+        'email' => 'mia.white@example.com',
+        'message' => 'Buongiorno, vorrei sapere se l\'appartamento dispone di posto auto.',
+        'apartment_id' => 47
+    ],
+    [
+        'name' => 'Jacob Red',
+        'email' => 'jacob.red@example.com',
+        'message' => 'Salve, sono interessato all\'appartamento. È possibile affittarlo arredato?',
+        'apartment_id' => 48
+    ],
+    [
+        'name' => 'Ella Black',
+        'email' => 'ella.black@example.com',
+        'message' => 'Ciao, vorrei sapere se l\'appartamento è situato in una zona tranquilla.',
+        'apartment_id' => 49
+    ],
+    [
+        'name' => 'William Blue',
+        'email' => 'william.blue@example.com',
+        'message' => 'Buonasera, vorrei sapere se l\'appartamento è vicino a una stazione della metropolitana.',
+        'apartment_id' => 50
+    ],
+    [
+        'name' => 'Ava Green',
+        'email' => 'ava.green@example.com',
+        'message' => 'Salve, sono interessata all\'appartamento. Quali sono i requisiti per il contratto di affitto?',
+        'apartment_id' => 51
+    ],
+    [
+        'name' => 'Noah Brown',
+        'email' => 'noah.brown@example.com',
+        'message' => 'Buongiorno, vorrei sapere se l\'appartamento è dotato di ascensore.',
+        'apartment_id' => 52
+    ],
+    [
+        'name' => 'Isabella Gray',
+        'email' => 'isabella.gray@example.com',
+        'message' => 'Ciao, sono interessata all\'appartamento. È possibile vedere delle foto?',
+        'apartment_id' => 53
+    ],
+    [
+        'name' => 'William Green',
+        'email' => 'william.green@example.com',
+        'message' => 'Salve, vorrei sapere se è possibile organizzare una visita per l\'appartamento.',
+        'apartment_id' => 54
+    ],
 ];
