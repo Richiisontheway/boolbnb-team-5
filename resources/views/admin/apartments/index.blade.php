c7be7697e93dad1824ba3e41e156e7494e5d0499
Richiisontheway <mineiriccardo@gmail.com>
2024-04-08 16:27:49 +0200

    edit card

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -32,85 +32,84 @@
             </div>
 
             <hr>
-                <div class="row">
-                    @foreach ($apartments as $singleApartment)
-                        <div class="col-12 col-md-6 col-lg-3 p-2">
-                            <div class="card h-100">
-                                @if ($singleApartment->cover_img != null)
-                                    <div class="img_container">
-                                        <img src="{{ $singleApartment->full_cover_img }}" class="card-img-top img-fluid" alt="{{$singleApartment->title}}" style="height: 200px; object-fit: cover;">
+            <div class="row">
+                @foreach ($apartments as $singleApartment)
+                    <div class="col-12 col-md-6 col-lg-3 p-2">
+                        <div class="card h-100">
+                            @if ($singleApartment->cover_img != null)
+                                <div class="img_container">
+                                    <img src="{{ $singleApartment->full_cover_img }}" class="card-img-top img-fluid" alt="{{$singleApartment->title}}" style="height: 200px; object-fit: cover;">
+                                </div>
+                            @endif
+                            <div class="card-body text-start">
+                                <div class="card-text row">
+                                    <div class="col-12">
+                                        <strong>
+                                            {{$singleApartment->title}}
+                                        </strong>
                                     </div>
-                                @endif
-                                <div class="card-body">
-                                    <div class="card-text row">
-                                        <div class="col-12">
-                                            <strong>
-                                                {{$singleApartment->title}}
-                                            </strong>
+                                    <div class="col-12 address_container">
+                                        {{ $singleApartment->address }}
+                                    </div>
+                                    <div class="d-flex justify-content-center col-12 g-2 ">
+                                        <div class="d-flex me-1 button_container">
+                                            <a href="{{ route('admin.apartments.show' , ['apartment' => $singleApartment->slug]) }}">
+                                                <i class="fa-solid fa-circle-info"></i>
+                                            </a>
                                         </div>
-                                        <div class="col-12 address_container">
-                                          {{ $singleApartment->address }}
+                                        <div class="d-flex me-1 button_container">
+                                            <a href="{{route('admin.apartments.edit' , ['apartment' => $singleApartment->slug  ])}}">
+                                                <i class="fa-solid fa-pencil"></i>
+                                            </a>
                                         </div>
-                                        <div class="d-flex justify-content-center col-12 g-2 ">
-                                            <div class="d-flex me-1 button_container">
-                                                <a href="{{ route('admin.apartments.show' , ['apartment' => $singleApartment->slug]) }}">
-                                                    <i class="fa-solid fa-circle-info"></i>
-                                                </a>
-                                            </div>
-                                            <div class="d-flex me-1 button_container">
-                                                <a href="{{route('admin.apartments.edit' , ['apartment' => $singleApartment->slug  ])}}">
-                                                    <i class="fa-solid fa-pencil"></i>
-                                                </a>
-                                            </div>
-                                            {{-- Bottone di eliminazione che apre una modale --}}
-                                            <div class="d-flex button_container" >
-                                                <button class="erase-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop-{{ $singleApartment->slug }}">
-                                                    <i class="fa-solid fa-eraser"></i>
-                                                </button>
-                                            </div>
-                                            {{-- Modale per l'eliminazione dell'appartamento --}}
-                                            <div class="modal fade" id="staticBackdrop-{{ $singleApartment->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">
-                                                                Eliminazione Appartamento
-                                                            </h1>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            Sei sicuto di voler eliminare: <b> {{ $singleApartment->title }} </b> ?
-                                                        </div>
-                                                        <div class="modal-footer">
-                
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                            
-                                                            <form 
-                                                            action="{{ route('admin.apartments.destroy', ['apartment' => $singleApartment]) }}" 
-                                                            method="POST">
-                                                            
-                                                            @csrf
+                                        {{-- Bottone di eliminazione che apre una modale --}}
+                                        <div class="d-flex button_container" >
+                                            <button class="erase-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop-{{ $singleApartment->slug }}">
+                                                <i class="fa-solid fa-eraser"></i>
+                                            </button>
+                                        </div>
+                                        {{-- Modale per l'eliminazione dell'appartamento --}}
+                                        <div class="modal fade" id="staticBackdrop-{{ $singleApartment->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                                                            Eliminazione Appartamento
+                                                        </h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Sei sicuto di voler eliminare: <b> {{ $singleApartment->title }} </b> ?
+                                                    </div>
+                                                    <div class="modal-footer">
+            
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                         
-                                                            @method('DELETE')
-                                                                <button 
-                                                                type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                                    Elimina
-                                                                </button>
-                                                            </form>
+                                                        <form 
+                                                        action="{{ route('admin.apartments.destroy', ['apartment' => $singleApartment]) }}" 
+                                                        method="POST">
+                                                        
+                                                        @csrf
+                                                    
+                                                        @method('DELETE')
+                                                            <button 
+                                                            type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                                Elimina
+                                                            </button>
+                                                        </form>
 
-                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                            
                                         </div>
+                                        
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @endforeach
-                </div>
-
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
     <script>
