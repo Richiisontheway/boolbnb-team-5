737fb07db6a1d4f0735535735750dca23d918e10
Richiisontheway <mineiriccardo@gmail.com>
2024-04-09 10:38:59 +0200

    edit apartment sponsorized

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -37,34 +37,47 @@
         <h2>
             Appartamenti sponsorizzati
         </h2>
-        <div class="row flex-wrap  sponsored_apartments_container">
-            @foreach ($sponsoredApartments as $apartment)
-            <div class="col-12 col-md-6 col-lg-3 p-2">
-                <div class="my-card h-100">
-                    @if ($apartment->cover_img != null)
-                        <img src="{{ $apartment->full_cover_img }}" alt="{{$apartment->title}}">
-                    @endif
-                    <h3>
-                        {{$apartment->title}}
-                    </h3>
-                    <p>
-                        {{ $apartment->city }}
-                        <br>
-                        {{ $apartment->address }}
-                    </p>
-                    <div>
-                        @php
-                            $firstSponsor = $apartment->sponsors->first();
-                        @endphp
-                        <p>Sponsorizzato: <span class="rounded-pill bg-primary text-light p-1">{{ $firstSponsor->title }}</span></p>                      
+        <div class="row sponsored_apartments_container">
+                @foreach ($sponsoredApartments as $singleApartment)
+                    <div class="col-12 col-md-6 col-lg-3 p-2">
+                        <div class="card h-100" style = "border:none">
+                            @if ($singleApartment->cover_img != null)
+                                <div class="img_container">
+                                    <img src="{{ $singleApartment->full_cover_img }}" class="card-img-top img-fluid" alt="{{$singleApartment->title}}" style="height: 200px; object-fit: cover;">
+                                </div>
+                            @endif
+                        <div class="card-body text-start">
+                            <div class="card-text row">
+                                <div class="col-12">
+                                    <strong>
+                                        {{$singleApartment->title}}
+                                    </strong>
+                                </div>
+                                <div class="col-12 address_container">
+                                    {{ $singleApartment->address }}
+                                </div>
+                                <div>
+                                    @php
+                                        $firstSponsor = $singleApartment->sponsors->first();
+                                    @endphp
+                                    <p> 
+                                        Sponsorizzato: 
+                                        <span class="rounded-pill sponsor-pill">
+                                            {{ $firstSponsor->title }}
+                                        </span>
+                                    </p>                      
+                                </div>
+                                <div class="d-flex justify-content-center col-12 g-2">
+                                    <div class="d-flex me-1 button_container">
+                                        <a href="{{ route('admin.apartments.show' , ['apartment' => $singleApartment->slug]) }}">
+                                            <i class="fa-solid fa-circle-info"></i>
+                                        </a>
+                                    </div>                                  
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="d-flex justify-content-around">
-                        <a href="{{ route('admin.apartments.show' , ['apartment' => $apartment->slug]) }}">
-                            <i class="fa-solid fa-circle-info"></i>
-                        </a>
-                    </div>      
                 </div>
-            </div>
             @endforeach
         </div>
 
