92ca99a15d61174d6dc272e8874a0e82a46aaf66
Richiisontheway <mineiriccardo@gmail.com>
2024-04-11 12:54:12 +0200

    responsive payment form

--- resources/views/admin/apartments/sponsor.blade.php
+++ resources/views/admin/apartments/sponsor.blade.php
@@ -3,22 +3,24 @@
 @section('page-title', 'Sponsorizzazioni disponibili')
 
 @section('main-content')
-    <h1 class="mb-4">{{ $apartmentTitle }}</h1>
-    <form id="payment-form" method="POST" action="{{ route('admin.sponsor.pay', $apartment_id) }}">
-        @csrf
-        <label class="mb-2" for="sponsor">Scegli il piano di sponsorizzazione:</label>
-        <select name="sponsor" id="sponsor" class="form-select" aria-label="Default select example">
-            @foreach($sponsors as $sponsor)
-                <option value="{{ $sponsor->id }}">{{ $sponsor->title }} - {{ $sponsor->price }}</option>
-            @endforeach
-        </select>
-        <div id="dropin-container"></div>
-        <input type="hidden" name="payment_method_nonce" id="payment-method-nonce" value="">
-        <button class="btn pay_button_sponsor" id="pay-button" type="submit" style="
-        background-color: #EA4C89;
-        color: white;
-        font-size: 1.2em;" >Paga</button>
-    </form>
+    <div class="row justify-content-center ">
+        <h1 class="mb-4 col-12 col-lg-8 text-left">{{ $apartmentTitle }}</h1>
+        <form id="payment-form" method="POST" action="{{ route('admin.sponsor.pay', $apartment_id) }}" class="col-12 col-lg-8">
+            @csrf
+            <label class="mb-2" for="sponsor">Scegli il piano di sponsorizzazione:</label>
+            <select name="sponsor" id="sponsor" class="form-select w-25" aria-label="Default select example">
+                @foreach($sponsors as $sponsor)
+                    <option value="{{ $sponsor->id }}">{{ $sponsor->title }} - {{ $sponsor->price }}</option>
+                @endforeach
+            </select>
+            <div id="dropin-container"></div>
+            <input type="hidden" name="payment_method_nonce" id="payment-method-nonce" value="">
+            <button class="btn pay_button_sponsor" id="pay-button" type="submit" style="
+            background-color: #EA4C89;
+            color: white;
+            font-size: 1.2em;" >Paga</button>
+        </form>
+    </div>
 
     <script src="https://js.braintreegateway.com/web/dropin/1.30.1/js/dropin.min.js"></script>
     <script>
