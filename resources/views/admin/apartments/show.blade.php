f58fe74ce61a0de6016ae77ac961304cbc25dc52
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-04-16 10:07:06 +0200

    Edit show apt

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -5,64 +5,69 @@
 @section('main-content')
     
     <div id="apartments-show" class="container mt-5">
-      
         <div class="row g-0">
-            
-                <div class="my_card_show " >
-                        
-                    <div class="img_container col-lg-6 col-md-12">
-                        <img src="{{ $apartment->full_cover_img }}" alt="{{$apartment->title}}">
+            <div class="my_card_show " >
+                <div class="img_container col-lg-6 col-md-12">
+                    <img src="{{ $apartment->full_cover_img }}" alt="{{$apartment->title}}">
+                </div>
+                <div class="my_card_show_body col-6 col-md-12">
+                    <div class="row g-0 my-3 text-warning">
+                        @if ($apartment->visible == 0)
+                            Attualmente hai scelto di impostare il tuo appartamento come non disponibile
+                        @endif
                     </div>
-                    
-                    <div class="my_card_show_body col-6 col-md-12">
-
-                        <div class="mt-3">
-                            <h4>
-                            {{$apartment->title}} 
-                            </h4>
-                            <div class="color-1">
-                                 {{ $apartment->address }}
+                    <div>
+                        <h4>
+                        {{$apartment->title}} 
+                        </h4>
+                        <div class="color-1">
+                                {{ $apartment->address }}
+                        </div>
+                        <p class="mt-2">
+                            {{ $apartment->n_rooms }} Stanze · {{ $apartment->n_beds }}<abbr title="letti"><i class="fa-solid fa-bed px-1"></i></abbr> · {{ $apartment->n_baths }} <abbr title="bagno"><i class="fa-solid fa-bath px-1 "></i></abbr> · {{ $apartment->mq }} m²
+                        </p>
+                    </div>
+                    <hr>
+                    {{-- link view statistiche --}}
+                    <div>
+                        <div>
+                            <h5 class="mb-2">
+                                Cosa Troverai
+                            </h5>
+                            <div class="row g-0">
+                                <div class="col-lg-10 col-md-12">
+                                    <div id="services">
+                                        @forelse ($apartment->services as $service)
+                                            <a href="{{ route('admin.services.show' , ['service' => $service->id]) }}" class="me-2" >
+                                                <i class="{{$service->icon}} pe-2"></i> {{$service->title}}
+                                            </a>
+                                        @empty
+                                            <h5>
+                                                Nessun servizio incluso
+                                            </h5>
+                                        @endforelse
+                                    </div>
+                                </div>
                             </div>
-                            <p>
-                                {{ $apartment->n_rooms }} Stanze · {{ $apartment->n_beds }}<abbr title="letti"><i class="fa-solid fa-bed px-1"></i></abbr> · {{ $apartment->n_baths }} <abbr title="bagno"><i class="fa-solid fa-bath px-1 "></i></abbr> · {{ $apartment->mq }} m²
-                            </p>
                         </div>
                         <hr>
-                        {{-- link view statistiche --}}
                         <div>
-                            <div>
-                                <h5>Cosa Troverai</h5>
-                                <div class="row">
-                                    <div class="col-lg-10 col-md-12">
-                                        <div id="services">
-                                            @forelse ($apartment->services as $service)
-                                                <a href="{{ route('admin.services.show' , ['service' => $service->id]) }}" class="me-2" >
-                                                    <i class="{{$service->icon}} pe-2"></i> {{$service->title}}
-                                                </a>
-                                            @empty
-                                                <h5>
-                                                    Nessun servizio incluso
-                                                </h5>
-                                            @endforelse
-                                        </div>
-                                    </div>
+                            {{-- Sezione sponsorizzazione  --}}
+                            <div class="row g-0 mb-3" id="sponsors">
+                                @if($latestSponsorship)
+                                <div class="col-auto sponsors-info">
+                                    Questo appartamento è già sponsorizzato: {{ $sponsorTitle }}. Fine sponsorizzazione: {{ $formattedDate }}
                                 </div>
-                                
+                                @else
+                                <div class="col-auto mb-3">
+                                    Massimizza le possibilità di affitto e
+                                    <a href="{{ route('admin.sponsor.show', $apartment->id) }}" class="text-decoration-none">SPONSORIZZA</a>
+                                    questo appartametno
+                                </div>
+                                @endif
                             </div>
-                            <hr>
-                            <div>
-                                {{-- Sezione sponsorizzazione  --}}
-                                <div>
-                                    @if($latestSponsorship)
-                                    <div id="sponsors" class="alert alert-info">
-                                        Questo appartamento è già sponsorizzato: {{ $sponsorTitle }}. Fine sponsorizzazione: {{ $formattedDate }}
-                                    </div>
-                                    @else
-                                    <a href="{{ route('admin.sponsor.show', $apartment->id) }}" class="text-decoration-none">Sponsorize this apartment</a>
-                                    @endif
-                                </div>
-                                <div class="d-flex">
-                                    {{-- tasto modifica --}}
+                            <div class="d-flex">
+                                {{-- tasto modifica --}}
                                 <div>
                                     <a class="btn  button" href="{{route('admin.apartments.edit' , ['apartment' => $apartment->slug  ])}}">
                                         <i class="fa-solid fa-pencil"></i>
@@ -74,50 +79,48 @@
                                         <i class="fa-solid fa-eraser"></i>
                                     </button>
         
-                                        <div class="modal fade" id="staticBackdrop-{{ $apartment->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">
-                                                            Eliminazione Appartamento
-                                                        </h1>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        Sei sicuto di voler eliminare: <b> {{ $apartment->title }} </b> ?
-                                                    </div>
-                                                    <div class="modal-footer">
-            
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            
-                                                        <form 
-                                                        action="{{ route('admin.apartments.destroy', ['apartment' => $apartment]) }}" 
-                                                        method="POST">
-                                                    
-                                                        @csrf
-                                                        @method('DELETE')
-                                                            <button 
-                                                            type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                                Elimina
-                                                            </button>
-                                                        </form>
-                                                    </div>
+                                    <div class="modal fade" id="staticBackdrop-{{ $apartment->slug }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                                                        Eliminazione Appartamento
+                                                    </h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Sei sicuro di voler eliminare: <b> {{ $apartment->title }} </b> ?
+                                                </div>
+                                                <div class="modal-footer">
+        
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+                                                    <form 
+                                                    action="{{ route('admin.apartments.destroy', ['apartment' => $apartment]) }}" 
+                                                    method="POST">
+                                                
+                                                    @csrf
+                                                    @method('DELETE')
+                                                        <button 
+                                                        type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                            Elimina
+                                                        </button>
+                                                    </form>
                                                 </div>
                                             </div>
-                                        </div>    
-                                    </div>      
-                                    <div>
-                                        <a href="{{ route('admin.apartments.statistics', $apartment->slug) }}" class="btn button">
-                                            <i class="fa-solid fa-envelope"></i>
-                                        </a>
-                                    </div>
+                                        </div>
+                                    </div>    
+                                </div>      
+                                <div>
+                                    <a href="{{ route('admin.apartments.statistics', $apartment->slug) }}" class="btn button">
+                                        <i class="fa-solid fa-envelope"></i>
+                                    </a>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
-            
+            </div>
         </div>
     </div>
 
